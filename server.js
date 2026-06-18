46a8738a46da2344f1d249c666134aba9667c0a0
Dragonraja2007 <likhit.1251071009@vit.edu>
2026-06-18 11:40:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('QueueGuide Backend is Running 🚀');
+});
 
 // In-memory data structure
 let lastToken = 0;
